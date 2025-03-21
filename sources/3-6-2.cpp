12ec26ec9ce35c68db6d379adaf7fa50047711b2
刘航宇 <2605172384@qq.com>
2025-03-21 13:04:07 +0000

    add sources/3-6-2.cpp.
    Signed-off-by: 刘航宇 <2605172384@qq.com>

---
+++ sources/3-6-2.cpp
@@ -0,0 +1,20 @@
+bool AddQ(Queue Q, ElementType X) {
+    if(Q->Count == Q->MaxSize) {
+        printf("Queue Full\n");
+        return false;
+    }
+
+    Q->Count++;
+    Q->Data[(Q->Front + Q->Count) % Q->MaxSize] = X;
+    return true;
+}
+
+ElementType DeleteQ(Queue Q) {
+    if(Q->Count == 0) {
+        printf("Queue Empty\n");
+        return ERROR;
+    }
+    Q->Count--;
+    Q->Front = (Q->Front + 1) % Q->MaxSize;
+    return Q->Data[Q->Front];
+} 
